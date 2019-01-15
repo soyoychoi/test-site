f091b0442aff0cd2948020e6b8de252eab9deae2
William Brower <will@mywisewallet.com>
2019-01-14 19:34:59 -0500

    Update SectionContact.jsx

--- src/views/AboutUsPage/Sections/SectionContact.jsx
+++ src/views/AboutUsPage/Sections/SectionContact.jsx
@@ -19,12 +19,32 @@ class SectionContact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      specialitySelect: "1"
+      email: "",
+      company: "",
+      name: ""
     };
+
+    this.onSubmit = this.onSubmit.bind(this);
   }
   handleSpeciality = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
+  handleChange = name => event => {
+  this.setState({[name]: event.target.value});
+};
+onSignIn() {
+    const {email, name, company} = this.state;
+    fetch("https://1d9lascwmj.execute-api.us-east-1.amazonaws.com/default/testSite", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({email: email, name: name, company: company})
+    }).then(response => response.json()).then(json => {
+      console.log('json', json);
+    });
+  }
   render() {
     const { classes } = this.props;
     return (
@@ -43,22 +63,31 @@ class SectionContact extends React.Component {
             </h4>
             <form>
               <GridContainer>
-                <GridItem md={6} sm={6}>
+                <GridItem md={4} sm={4}>
                   <CustomInput
                     labelText="Your name"
                     formControlProps={{
                       fullWidth: true
                     }}
+                    value={this.state.name} onChange={this.handleChange("name")}
                   />
                 </GridItem>
-                <GridItem md={6} sm={6}>
+                <GridItem md={4} sm={4}>
                   <CustomInput
                     labelText="Your email"
                     formControlProps={{
                       fullWidth: true
-                    }}
+                    }} value={this.state.email} onChange={this.handleChange("email")}
                   />
                 </GridItem>
+                <GridItem md={4} sm={4}>
+                  <CustomInput
+                    labelText="Your company"
+                    formControlProps={{
+                      fullWidth: true
+                    }} value={this.state.email} onChange={this.handleChange("company")}
+                  />
+              </GridItem>
               </GridContainer>
               <GridContainer>
                 <GridItem
@@ -70,7 +99,7 @@ class SectionContact extends React.Component {
                     classes.textCenter
                   )}
                 >
-                  <Button color="success" round>
+                  <Button color="success" round onClick={this.onSubmit}>
                     Let's talk
                   </Button>
                 </GridItem>
